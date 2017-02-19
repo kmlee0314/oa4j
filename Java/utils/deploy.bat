45e400853ea37911ad741d323aa718d311061e03
Andreas Vogler <andreas.vogler@me.com>
2017-02-19 18:38:37 +0100

    Refactoring

--- Java/utils/deploy.bat
+++
@@ -1,7 +0,0 @@
-copy target\*.jar ..\Project\Example\lib
-del ..\Project\Example\classes\*.class
-copy target\test-classes\*.class ..\Project\Example\classes
-
-copy target\*.jar C:\WinCC_OA_Proj\Example\lib
-del C:\WinCC_OA_Proj\Example\classes\*.class
-copy target\test-classes\*.class C:\WinCC_OA_Proj\Example\classes

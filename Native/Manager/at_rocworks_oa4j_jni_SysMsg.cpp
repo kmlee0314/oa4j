d14059fe6885f86a85bc3f0bb27dc886b7c05243
Andreas Vogler <andreas.vogler@me.com>
2017-02-21 08:53:09 +0100

    CtrlExt Test-Panel

---
+++ Native/Manager/at_rocworks_oa4j_jni_SysMsg.cpp
@@ -0,0 +1,9 @@
+#include <at_rocworks_oa4j_jni_SysMsg.h>
+#include <WCCOAJavaManager.hxx>
+
+JNIEXPORT jint JNICALL Java_at_rocworks_oa4j_jni_SysMsg_getSysMsgType
+(JNIEnv *, jobject, jlong cptr)
+{
+	SysMsg *v = (SysMsg*)(cptr);
+	return v->getSysMsgType();
+}

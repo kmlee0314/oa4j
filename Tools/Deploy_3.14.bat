af7ea161e87245afc3671fd685d2aa4ffac54000
Andreas Vogler <andreas.vogler@me.com>
2017-03-26 21:09:11 +0200

    moved binaries

--- Tools/Deploy_3.14.bat
+++ Tools/Deploy_3.14.bat
@@ -1,15 +1,15 @@
-copy ..\Native\Manager\bin\WCCOAjava.dll   ..\Project\Example\bin-3.14\
-copy ..\Native\Manager\bin\WCCOAjava.exe   ..\Project\Example\bin-3.14\
+copy ..\Native\Manager\bin\WCCOAjava.dll   ..\Native\Version\3.14\
+copy ..\Native\Manager\bin\WCCOAjava.exe   ..\Native\Version\3.14\
 
-copy ..\Native\Driver\bin\WCCOAjavadrv.dll ..\Project\Example\bin-3.14\
-copy ..\Native\Driver\bin\WCCOAjavadrv.exe ..\Project\Example\bin-3.14\
+copy ..\Native\Driver\bin\WCCOAjavadrv.dll ..\Native\Version\3.14\
+copy ..\Native\Driver\bin\WCCOAjavadrv.exe ..\Native\Version\3.14\
 
-copy ..\Native\CtrlExt\bin\JavaCtrlExt.dll ..\Project\Example\bin-3.14\
+copy ..\Native\CtrlExt\bin\JavaCtrlExt.dll ..\Native\Version\3.14\
 
-copy ..\Java\target\WCCOAjava.jar   ..\Project\Example\bin\
+copy ..\Java\bin\WCCOAjava.jar      ..\Project\Example\bin\
 copy ..\Java\target\test-classes\*  ..\Project\Example\bin\
 
-copy ..\Project\Example\bin-3.14\* C:\WinCC_OA_Proj\Example-3.14\bin\
+copy ..\Native\Version\3.14\*      C:\WinCC_OA_Proj\Example-3.14\bin\
 copy ..\Project\Example\bin\*      C:\WinCC_OA_Proj\Example-3.14\bin\
 
 pause
--- Tools/Deploy_3.15.bat
+++ Tools/Deploy_3.15.bat
@@ -1,15 +1,15 @@
-copy ..\Native\Manager\bin\WCCOAjava.dll   ..\Project\Example\bin-3.15\
-copy ..\Native\Manager\bin\WCCOAjava.exe   ..\Project\Example\bin-3.15\
+copy ..\Native\Manager\bin\WCCOAjava.dll   ..\Native\Version\3.15\
+copy ..\Native\Manager\bin\WCCOAjava.exe   ..\Native\Version\3.15\
 
-copy ..\Native\Driver\bin\WCCOAjavadrv.dll ..\Project\Example\bin-3.15\
-copy ..\Native\Driver\bin\WCCOAjavadrv.exe ..\Project\Example\bin-3.15\
+copy ..\Native\Driver\bin\WCCOAjavadrv.dll ..\Native\Version\3.15\
+copy ..\Native\Driver\bin\WCCOAjavadrv.exe ..\Native\Version\3.15\
 
-copy ..\Native\CtrlExt\bin\JavaCtrlExt.dll ..\Project\Example\bin-3.15\
+copy ..\Native\CtrlExt\bin\JavaCtrlExt.dll ..\Native\Version\3.15\
 
-copy ..\Java\target\WCCOAjava.jar   ..\Project\Example\bin\
+copy ..\Java\bin\WCCOAjava.jar      ..\Project\Example\bin\
 copy ..\Java\target\test-classes\*  ..\Project\Example\bin\
 
-copy ..\Project\Example\bin-3.15\* C:\WinCC_OA_Proj\Example_3.15\bin\
-copy ..\Project\Example\bin\*      C:\WinCC_OA_Proj\Example_3.15\bin\
+copy ..\Native\Version\3.15\*      C:\WinCC_OA_Proj\Example-3.15\bin\
+copy ..\Project\Example\bin\*      C:\WinCC_OA_Proj\Example-3.15\bin\
 
 pause

cf3e41e11c4def3fc741b4978c79e4a1dfc2bb53
Andreas Vogler <andreas.vogler@me.com>
2017-03-26 22:07:35 +0200

    moved binaries to bundle directory

--- Tools/Deploy_3.14.bat
+++ Tools/Deploy_3.14.bat
@@ -1,10 +1,12 @@
-copy ..\Native\Manager\bin\WCCOAjava.dll   ..\Native\Version\3.14\
-copy ..\Native\Manager\bin\WCCOAjava.exe   ..\Native\Version\3.14\
+copy ..\Native\Manager\bin\WCCOAjava.dll   ..\Bundle\3.14\
+copy ..\Native\Manager\bin\WCCOAjava.exe   ..\Bundle\3.14\
 
-copy ..\Native\Driver\bin\WCCOAjavadrv.dll ..\Native\Version\3.14\
-copy ..\Native\Driver\bin\WCCOAjavadrv.exe ..\Native\Version\3.14\
+copy ..\Native\Driver\bin\WCCOAjavadrv.dll ..\Bundle\3.14\
+copy ..\Native\Driver\bin\WCCOAjavadrv.exe ..\Bundle\3.14\
 
-copy ..\Native\CtrlExt\bin\JavaCtrlExt.dll ..\Native\Version\3.14\
+copy ..\Native\CtrlExt\bin\JavaCtrlExt.dll ..\Bundle\3.14\
+
+copy ..\Java\bin\WCCOAjava.jar      ..\Bundle\3.14\
 
 copy ..\Java\bin\WCCOAjava.jar      ..\Project\Example\bin\
 copy ..\Java\target\test-classes\*  ..\Project\Example\bin\
--- Tools/Deploy_3.15.bat
+++ Tools/Deploy_3.15.bat
@@ -1,15 +1,17 @@
-copy ..\Native\Manager\bin\WCCOAjava.dll   ..\Native\Version\3.15\
-copy ..\Native\Manager\bin\WCCOAjava.exe   ..\Native\Version\3.15\
+copy ..\Native\Manager\bin\WCCOAjava.dll   ..\Bundle\3.15\
+copy ..\Native\Manager\bin\WCCOAjava.exe   ..\Bundle\3.15\
 
-copy ..\Native\Driver\bin\WCCOAjavadrv.dll ..\Native\Version\3.15\
-copy ..\Native\Driver\bin\WCCOAjavadrv.exe ..\Native\Version\3.15\
+copy ..\Native\Driver\bin\WCCOAjavadrv.dll ..\Bundle\3.15\
+copy ..\Native\Driver\bin\WCCOAjavadrv.exe ..\Bundle\3.15\
 
-copy ..\Native\CtrlExt\bin\JavaCtrlExt.dll ..\Native\Version\3.15\
+copy ..\Native\CtrlExt\bin\JavaCtrlExt.dll ..\Bundle\3.15\
+
+copy ..\Java\bin\WCCOAjava.jar      ..\Bundle\3.15\
 
 copy ..\Java\bin\WCCOAjava.jar      ..\Project\Example\bin\
 copy ..\Java\target\test-classes\*  ..\Project\Example\bin\
 
-copy ..\Native\Version\3.15\*      C:\WinCC_OA_Proj\Example-3.15\bin\
+copy ..\Native\Version\3.14\*      C:\WinCC_OA_Proj\Example-3.15\bin\
 copy ..\Project\Example\bin\*      C:\WinCC_OA_Proj\Example-3.15\bin\
 
 pause

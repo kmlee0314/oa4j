1a1e8c6e851519fd968c39a84dc7fd045e132f13
Andreas Vogler <andreas.vogler@me.com>
2017-03-27 17:56:32 +0200

    scala test program

--- Scala/src/Test.scala
+++ Scala/src/Test.scala
@@ -1,6 +1,5 @@
 import at.rocworks.oa4j.base._
 import at.rocworks.oa4j.`var`._
-import at.rocworks.oa4j.utils.Debug
 
 /**
   * Created by vogler on 2/21/2017.
@@ -11,27 +10,27 @@ object Test {
     // logs are printed to WCCOAjava<num>.0.log and WCCOAjava10.err
     val m = (new JManager).init(args)
     m.start()
-    Debug.out.info("runSet...")
+    JDebug.out.info("runSet...")
     runSet()
-    Debug.out.info("runGet...")
+    JDebug.out.info("runGet...")
     runGet()
-    Debug.out.info("runConnect...")
+    JDebug.out.info("runConnect...")
     runConnect()
-    Debug.out.info("stop...")
+    JDebug.out.info("stop...")
     m.stop()
-    Debug.out.info("done.")
+    JDebug.out.info("done.")
   }
 
   def runGet(): Unit = {
     val v1 = JClient.dpGet.add("ExampleDP_Trend1.").await()
-    Debug.out.info("v1: "+v1.toString)
+    JDebug.out.info("v1: "+v1.toString)
 
     val v2 = JClient.dpGet("ExampleDP_Trend1.");
-    Debug.out.info("v2: "+v2.toString)
+    JDebug.out.info("v2: "+v2.toString)
 
     val v3var = new VariablePtr()
     val v3ret = JClient.dpGet("ExampleDP_Trend1.", v3var);
-    Debug.out.info(s"v3: ret=$v3ret value="+v3var.get.toString)
+    JDebug.out.info(s"v3: ret=$v3ret value="+v3var.get.toString)
   }
 
   def runSet(): Unit = {
@@ -52,28 +51,28 @@ object Test {
   }
 
   def runConnect() {
-    Debug.out.info("dpConnect...")
+    JDebug.out.info("dpConnect...")
 
     val conn = JClient.dpConnect.add("ExampleDP_Trend1.")
 
     conn.answer((answer) => {
-        Debug.out.info("--- ANSWER BEG ---")
-        Debug.out.info(answer.toString())
-        Debug.out.info("--- ANSWER END ---")
+        JDebug.out.info("--- ANSWER BEG ---")
+        JDebug.out.info(answer.toString())
+        JDebug.out.info("--- ANSWER END ---")
       }
     )
 
     conn.hotlink((hotlink) => {
-        Debug.out.info("--- HOTLINK BEG ---")
-        Debug.out.info(hotlink.toString())
-        Debug.out.info("--- HOTLINK END ---")
+        JDebug.out.info("--- HOTLINK BEG ---")
+        JDebug.out.info(hotlink.toString())
+        JDebug.out.info("--- HOTLINK END ---")
       }
     )
 
     conn.connect()
-    Debug.out.info("sleep...")
+    JDebug.out.info("sleep...")
     Thread.sleep(1000 * 60)
-    Debug.out.info("done")
+    JDebug.out.info("done")
     conn.disconnect()
   }
 }
